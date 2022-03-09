5ffa93e66aaf5cc18b3a8e27002c417a37633338
DanielTorres28 <daniel.vic1@hotmail.com>
2022-03-08 23:16:17 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj02_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensagem = "\n\nBoa Noite", nome; // Declaração de Variáveis 
+            Console.WriteLine(DateTime.Now);
+            mensagem = "\n\nBoa Noite"; // Atribuição de Valor 
+            nome = "Daniel"; // Atribuição de Valor   
+            Console.WriteLine(mensagem +" "+ nome);
+            Console.Write("\n\nRepresentantes do 1bds: " + nome + " e "); // os caracteres \n (arrab n) corresponde espaçamento de linhas 
+            nome = "Carlos";
+            Console.WriteLine(nome);
+            Console.WriteLine(mensagem + " " + nome);
+                //Console.Writeline("\n\nRepresentantes do 1bds: " + nome +" e " + nome);
+            nome = "";
+            Console.Write("Pressione qualquer tecla para encerrar este programa...");
+            Console.ReadKey();
+        }
+    }
+}
